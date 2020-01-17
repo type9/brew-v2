82e35248e3de31a9984d956a29e671a645b7e5c4
type9 <gabespersonal@gmail.com>
2020-01-17 03:01:59 -0800

    Successfully rendered react through flask in preparation for web deployment

--- flask-server/app.py
+++ flask-server/app.py
@@ -4,6 +4,6 @@ app = flask.Flask("__main__")
 
 @app.route("/")
 def index():
-    return flask.render_template("index.html", token="reactflask")
+    return flask.render_template("index.html", token="flaskreact")
 
 app.run(debug=True)
