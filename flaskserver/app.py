02a29416a8b4e36fb91f0391608e9fccb9c92faa
Type9 <gabespersonal@gmail.com>
2020-05-13 13:36:42 -0700

    Deployment fixes

--- flaskserver/app.py
+++ flaskserver/app.py
@@ -51,4 +51,4 @@ def get_healthcheck():
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host= '0.0.0.0')
