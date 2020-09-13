d5b43b5d0339590e1f70c13068e4e37af111b1fa
Type9 <gabespersonal@gmail.com>
2020-09-12 18:31:09 -0700

    Deployment fixes

---
+++ flaskserver/static/react/precache-manifest.3291cea704f390c4be724b34c4432d87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d9a8b2a9e5ed53f0d6afb24952e136c",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "e2780ec9b2d4ccc76a4f",
+    "url": "/static/react/js/2.01fe365f.chunk.js"
+  },
+  {
+    "revision": "2116ba47e9f87a12c285c39245415b5b",
+    "url": "/static/react/js/2.01fe365f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3228e19b89bdc3b59031",
+    "url": "/static/react/js/main.59223bb0.chunk.js"
+  },
+  {
+    "revision": "93e9bd82758163cbfb7e",
+    "url": "/static/react/js/runtime-main.87943426.js"
+  },
+  {
+    "revision": "1be27cfdda9e71650b6b3cb7c74ecc34",
+    "url": "/static/react/media/logo.1be27cfd.svg"
+  }
+]);
